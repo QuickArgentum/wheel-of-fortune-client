eb10931842458bcde364a1e35e1f18c6536aecbe
QuickArgentum <the.keksik.is.a.lie@gmail.com>
2019-01-26 14:22:53 +0200

    Fixed network connector config. Fixed scene structure problems.

--- assets/src/NetworkConnector.ts
+++ assets/src/NetworkConnector.ts
@@ -3,23 +3,21 @@ const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class NetworkConnector {
-    private readonly axiosConfig = {
-        baseURL: '127.0.0.1:3000/',
-        timeout: 1000
-    }
+    
+    private readonly baseURL = "http://127.0.0.1:3000"
 
-    private async getScore(username: string) : Promise<any> {
-        const res = await axios.get("score?user=" + username, this.axiosConfig);
+    public async getScore(username: string) : Promise<any> {
+        const res = await axios.get(this.baseURL + "/score?user=" + username);
         return res;
     }
 
-    private async getSegments() : Promise<any> {
-        const res = await axios.get("segments", this.axiosConfig);
+    public async getSegments() : Promise<any> {
+        const res = await axios.get(this.baseURL + "/segments");
         return res;
     }
 
-    private async getSpin(username: string) : Promise<any> {
-        const res = await axios.get("spin?user=" + username, this.axiosConfig);
+    public async getSpin(username: string) : Promise<any> {
+        const res = await axios.get(this.baseURL + "/spin?user=" + username);
         return res;
     }
 }
