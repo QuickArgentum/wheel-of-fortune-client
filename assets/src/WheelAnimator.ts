1ed103e4db7cdb93c4be282ba19587d84819ee39
QuickArgentum <the.keksik.is.a.lie@gmail.com>
2019-01-27 11:54:01 +0200

    Added visual shadows to wheel elements. Added a pointer to the wheel.

--- assets/src/WheelAnimator.ts
+++ assets/src/WheelAnimator.ts
@@ -13,8 +13,17 @@ export default class WheelAnimator extends cc.Component {
     @property
     maxFullRevolutions: number = 6;
 
+    @property
+    pointerFadeInDuration: number = 2.0;
+
     @property(cc.Node)
     backgroundStar: cc.Node = null;
+
+    @property(cc.Node)
+    wheelShadow: cc.Node = null;
+
+    @property(cc.Node)
+    wheelPointer: cc.Node = null;
 
     private ui: UIManager;
 
@@ -25,17 +34,25 @@ export default class WheelAnimator extends cc.Component {
 
         let rotate = cc.rotateBy(angle * this.secondsPerRevolution / 360, -angle);
         rotate.easing(cc.easeElasticOut(3.0));
+        let sequence = cc.sequence(rotate, cc.callFunc(selector, selectorTarget));
         let rotateStar = cc.rotateBy(angle * this.secondsPerRevolution / 360, -angle / 8);
         rotateStar.easing(cc.easeElasticOut(3.0));
-        let sequence = cc.sequence(rotate, cc.callFunc(selector, selectorTarget));
+        let fadePointer = cc.fadeIn(this.pointerFadeInDuration);
+
         this.node.runAction(sequence);
+        this.wheelShadow.runAction(rotate.clone());
         this.backgroundStar.runAction(rotateStar);
+        this.wheelPointer.runAction(fadePointer);
     }
 
     public resetWheel(selector: Function, selectorTarget: any) {
         let rotate = cc.rotateTo(0.5, 0.0);
         rotate.easing(cc.easeElasticInOut(3.0));
         let sequence = cc.sequence(rotate, cc.callFunc(selector, selectorTarget));
+        let fadePointer = cc.fadeOut(0.5);
+
+        this.wheelShadow.runAction(rotate.clone());
         this.node.runAction(sequence);
+        this.wheelPointer.runAction(fadePointer);
     }
 }
